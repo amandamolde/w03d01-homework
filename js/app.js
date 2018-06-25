d206c85f5598dab8fd6f30518ebf1e4327528135
Amanda Molde <amanda.molde@gmail.com>
2018-06-25 15:31:42 -0600

    Created a Class for my tomagotchi

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 $('document').ready(function () {
-	console.log('working');
+	// console.log('working');
+
+	class Tomagotchi {
+		constructor(hunger, sleepiness, boredom, age){
+			this.hunger = 1;
+			this.sleepiness = 1;
+			this.boredom = 1;
+			this.age = 1;
+		}
+	};
 
 
 
