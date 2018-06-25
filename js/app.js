95818f30ce4936f1bee6c433237a4dcc5c40b6e1
Amanda Molde <amanda.molde@gmail.com>
2018-06-25 16:37:03 -0600

    Started setting up buttons, still need to work on updating display of stats

--- js/app.js
+++ js/app.js
@@ -15,10 +15,10 @@ $('document').ready(function () {
 const pet = new Tomagotchi(1, 1, 1, 1);
 // console.log(pet, ' this is my Tomagotchi');
 
-let hunger = 1;
-let sleepiness = 1;
-let boredom = 1;
-let age = 1;
+let hunger = 6;
+let sleepiness = 4;
+let boredom = 10;
+let age = 6;
 
 const displayMetrics = () => {
 	$('#hunger').text('Hunger: ' + hunger);
@@ -26,6 +26,30 @@ const displayMetrics = () => {
 	$('#boredom').text('Boredom: ' + boredom);
 	$('#age').text('Age: ' + age);
 };
+
+$('#feed').on('click', (e) => {
+	// console.log('feed clicked')
+	if(hunger > 1) {
+		hunger --;
+		// console.log(hunger);
+	}
+});
+
+$('#lights').on('click', (e) => {
+	// console.log('lights clicked')
+	if (sleepiness > 1) {
+		sleepiness --;
+		// console.log(sleepiness);
+	}
+});
+
+$('#play').on('click', (e) => {
+	// console.log('play clicked')
+	if (boredom > 1) {
+		boredom --;
+		// console.log(boredom);
+	}
+});
 
 displayMetrics();
 
