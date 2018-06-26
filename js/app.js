58630400fa0df63f9e93c0759605c4e96e60733c
Amanda Molde <amanda.molde@gmail.com>
2018-06-25 20:20:26 -0600

    Increase pet's age by one year every one minute

--- js/app.js
+++ js/app.js
@@ -10,48 +10,58 @@ $('document').ready(function () {
 	};
 
 
-const pet = new Tomagotchi(1, 1, 1, 1);
+	const pet = new Tomagotchi(1, 1, 1, 1);
 
-let hunger = 8;
-let sleepiness = 4;
-let boredom = 10;
-let age = 6;
+	let hunger = 8;
+	let sleepiness = 4;
+	let boredom = 10;
+	let age = 6;
 
-const displayMetrics = () => {
-	$('#hunger').text('Hunger: ' + hunger);
-	$('#sleepiness').text('Sleepiness: ' + sleepiness);
-	$('#boredom').text('Boredom: ' + boredom);
-	$('#age').text('Age: ' + age);
-};
-
-$('#feed').on('click', (e) => {
-	if(hunger > 1) {
-		hunger --;
+	const displayMetrics = () => {
 		$('#hunger').text('Hunger: ' + hunger);
-	}
-});
-
-$('#lights').on('click', (e) => {
-	if (sleepiness > 1) {
-		sleepiness --;
 		$('#sleepiness').text('Sleepiness: ' + sleepiness);
-	}
-});
-
-$('#play').on('click', (e) => {
-	if (boredom > 1) {
-		boredom --;
 		$('#boredom').text('Boredom: ' + boredom);
-	}
-});
+		$('#age').text('Age: ' + age);
+	};
 
-$('#submitName').on('click', (e) => {
-	const $nameInput = $('input').val();
-	$(e.currentTarget).parent().remove();
-	const $name = $('<h1/>').text($nameInput);
-	$('body').prepend($name);
-})
+	$('#feed').on('click', (e) => {
+		if(hunger > 1) {
+			hunger --;
+			$('#hunger').text('Hunger: ' + hunger);
+		}
+	});
 
-displayMetrics();
+	$('#lights').on('click', (e) => {
+		if (sleepiness > 1) {
+			sleepiness --;
+			$('#sleepiness').text('Sleepiness: ' + sleepiness);
+		}
+	});
+
+	$('#play').on('click', (e) => {
+		if (boredom > 1) {
+			boredom --;
+			$('#boredom').text('Boredom: ' + boredom);
+		}
+	});
+
+
+	$('#submitName').on('click', (e) => {
+		const $nameInput = $('input').val();
+		$(e.currentTarget).parent().remove();
+		const $name = $('<h1/>').text($nameInput);
+		$('body').prepend($name);
+	});
+
+	const increaseAge = () => {
+		setInterval(function () {
+			age ++;
+			$('#age').text('Age: ' + age);
+		}, 60000);
+	};
+	
+
+	displayMetrics();
+	increaseAge();
 
 })
